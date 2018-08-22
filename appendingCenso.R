6077a6786d882ecc270e985ce122c4b86ef810b1
luucasac <lucas.alvarenga92@gmail.com>
2018-08-21 21:16:54 -0300

    Appending all years data sets of 'censo escolar'.

--- appendingCenso.R
+++ appendingCenso.R
@@ -5,20 +5,23 @@
 
 library(tidyverse)
 
-appendCenso <- function(ano) {
-  
-  
-  
-  for(i in seq_along(ano)){
-    data <- read.csv2(paste0("censo_escolar",
-                             ano[i],
-                             ".csv")) %>% 
-      bind_rows(data)
-    
-    write.csv2(data, "censo_escolarFinal.csv")
-    
-    }
+datalist = list() # CREATING A EMPTY LIST TO RECEIVE EVERY DATA.FRAME OF CENSO
 
+ano <- c(2007:2015) # INPUTING MANUALLY MY PARAMETERS
+
+for (i in seq_along(ano)) {
+  
+  # READING CSV FILES FROM EACH YEAR OF CENSO
+  
+  dat <- read.csv2(paste0("censo_escolar", ano[i], ".csv"))
+  
+  # INPUTTING DATA INTO A LIST
+  
+  datalist[[i]] <- dat 
+  
+  # BINDING ALL DATA.FRAMES IN THE LIST
+  
+  r <- bind_rows(datalist)
+  
+  
 }
-
-appendCenso(c(2007:2015))
